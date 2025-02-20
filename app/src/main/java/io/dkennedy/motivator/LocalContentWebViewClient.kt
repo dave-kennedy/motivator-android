0ec5d9c6a33c437b8f4947cfb4015799f42a25b7
Dave Kennedy <dave-kennedy@users.noreply.github.com>
2025-02-19 22:28:29 -0700

    Fix import/export

--- app/src/main/java/io/dkennedy/motivator/LocalContentWebViewClient.kt
+++ app/src/main/java/io/dkennedy/motivator/LocalContentWebViewClient.kt
@@ -1,13 +1,25 @@
 package io.dkennedy.motivator
 
+import android.content.Intent
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.webkit.WebViewAssetLoader
 import androidx.webkit.WebViewClientCompat
 
 class LocalContentWebViewClient(private val assetLoader: WebViewAssetLoader) : WebViewClientCompat() {
     override fun shouldInterceptRequest(view: WebView, request: WebResourceRequest): WebResourceResponse? {
         return assetLoader.shouldInterceptRequest(request.url)
+    }
+
+    override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+        if (request.url.host == "appassets.androidplatform.net") {
+            return false
+        }
+
+        val intent = Intent(Intent.ACTION_VIEW, request.url)
+        startActivity(view.context, intent, null)
+        return true
     }
 }
