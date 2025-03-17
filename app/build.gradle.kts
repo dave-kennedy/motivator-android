ec72684557d5900f9b0fb3d3ab85f18cdbac4b28
Dave Kennedy <dave-kennedy@users.noreply.github.com>
2025-03-17 17:07:03 -0600

    Update deps and bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "io.dkennedy.motivator"
         minSdk = 25
         targetSdk = 35
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
